ac085ae44b69bf31d57257bbe3975e496aa25b42
Olof Johansson <olof@oddhill.se>
2011-03-25 14:21:56 +0100

    Fixed a syntax error on the main js file.

--- profiles/odddrupal/themes/oddhill/js/oddhill.js
+++ profiles/odddrupal/themes/oddhill/js/oddhill.js
@@ -3,8 +3,8 @@
 Drupal.behaviors.oddhill = {
   attach: function (context, settings) {
     // Put focused class on every form.
-  $('form').focusedForm();
+    $('form').focusedForm();
   }
 }
 
-}(jQuery);
+})(jQuery);
