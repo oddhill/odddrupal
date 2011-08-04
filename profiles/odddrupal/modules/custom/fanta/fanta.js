ce06c7d58655902c74a67e841d8fb2d696a98b24
Olof Johansson <olof@oddhill.se>
2011-08-04 18:27:09 +0200

    Improved Fanta.
    - The browser and version check is now being handled by jQuery instead of server side. This should ensure correct handling.

--- profiles/odddrupal/modules/custom/fanta/fanta.js
+++ profiles/odddrupal/modules/custom/fanta/fanta.js
@@ -1,8 +1,11 @@
 (function($) {
 
-  Drupal.behaviors.ie6Tsunami = {
+  Drupal.behaviors.fanta = {
     attach: function (context, settings) {
-      e(settings.fanta.ie6Path);
+      if ($.browser.msie && $.browser.version < 7) {
+        // Popup if the browser is IE and the version is smaller than 7.
+        e(settings.fanta.ie6Path);
+      }
     }
   }
   
