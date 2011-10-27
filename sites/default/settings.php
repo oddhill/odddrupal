d459e349a404c6521fee37d9b3b7e184623bd340
Olof Johansson <olof@oddhill.se>
2011-10-27 08:50:54 +0200

    Drupal 7.9
    - Also updated our settings.php file to include the new fast 404 settings.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -26,6 +26,13 @@ ini_set('session.cookie_lifetime', 2000000);
 ini_set('error_reporting', E_ALL ^ E_NOTICE);
 
 /**
+ * Fast 404 settings.
+ */
+$conf['404_fast_paths_exclude'] = '/\/(?:styles)\//';
+$conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
+$conf['404_fast_html'] = '<html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
+
+/**
  * Include the local settings file.
  *
  * Do not remove this line, since it includes
