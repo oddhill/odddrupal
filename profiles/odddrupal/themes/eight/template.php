3d15f148f06d1fedbe66692749ef73fa40366b3e
Olof Johansson <olof@oddhill.se>
2013-02-21 14:04:43 +0100

    Added default views for the content and user administration pages. Created a default administration theme that extends Seven.

---
+++ profiles/odddrupal/themes/eight/template.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ * Main PHP file for this theme.
+ *
+ * This theme will be used as the default administration theme, and uses Seven
+ * as the base theme. Any overrides or additions should be placed here.
+ */
