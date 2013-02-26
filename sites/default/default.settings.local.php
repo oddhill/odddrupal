6da55283d216ad85b3cc5ec7c404cafc1b3653e6
Olof Johansson <olof@oddhill.se>
2013-02-26 14:13:15 +0100

    Added default settings for Stage File Proxy.

--- sites/default/default.settings.local.php
+++ sites/default/default.settings.local.php
@@ -18,3 +18,17 @@ $databases['default']['default'] = array(
   'password' => '',
   'host' => 'dev',
 );
+
+/**
+ * Configuration for Stage File Proxy.
+ *
+ * These settings should be enabled when the staging or production environment
+ * exists. The stage_file_proxy_origin settings should always point to to
+ * highest environment in the food chain, which means that it should point to
+ * the production site if it exists, or the staging site if a production site
+ * doesn't exist yet.
+ *
+ * Don't forget to remove the comments when you've configured this!
+ */
+#$conf['stage_file_proxy_origin'] = 'http://www.example.com';
+#$conf['stage_file_proxy_hotlink'] = TRUE;
