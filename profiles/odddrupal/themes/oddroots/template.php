fcaaf0fa2a39960b4d2dee3d6b3617e5d9e60bec
Olof Johansson <olof@oddhill.se>
2013-06-25 10:04:33 +0200

    Removed Odd Roots theme.
    Before merging this commit, make sure that the site isn't using Odd Roots as the base theme. If it is, copy Odd Roots to the sites/all/themes folder before merging.

--- profiles/odddrupal/themes/oddroots/template.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function oddroots_css_alter(&$css) {
-  $exclude = array(
-    'modules/system/system.menus.css' => FALSE,
-    'modules/system/system.theme.css' => FALSE,
-  );
-  $css = array_diff_key($css, $exclude);
-}
