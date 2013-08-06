009ca61266b2e81fb792db393bea7cde0f971569
Olof Johansson <olof@oddhill.se>
2013-08-06 13:43:00 +0200

    Removed the script files for SQL pull and push.
    The syncing between development and staging is now available via Drush instead.
    
    To make this available for your computer, copy and paste the following in the terminal:
    cp -r ~/Dropbox/Program\ \&\ Filer/Drupal/staging_sync ~/.drush/
    
    Then run "drush cc drush" to clear Drush's cache.
    
    The commands that you should use are "drush push" and "drush pull".

--- sites/default/sql-pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-drush sql-sync @staging default --yes
-drush ts development
-drush fra --yes
-drush cc all
--- sites/default/sql-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-drush sql-sync default @staging --yes
-drush @staging ts staging
-drush @staging fra --yes
-drush @staging cc all
