f99c75942344c4f02848e0972af03438a6a2ea83
Olof Johansson <olof.johansson@oddhill.se>
2014-07-17 09:20:19 +0200

    Removed the settings.site.php.default file.
    Since this file only contains configuration for Stage File Proxy, which isn't needed anymore, there's no reason to include a default file for these type of settings.
    
    It's still possible to create one if needed though, as it will be included if it exists.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -28,9 +28,11 @@ $conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl
 $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 
 /**
- * Include the site specific settings file.
+ * Include the site specific settings file, if it exists.
  */
-include 'settings.site.php';
+if (file_exists('settings.site.php')) {
+  include 'settings.site.php';
+}
 
 /**
  * Include the local settings file.
