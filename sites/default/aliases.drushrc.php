003bbd1a577c4a63331b9624a12d36c2a66cad08
Olof Johansson <olof.johansson@oddhill.se>
2014-10-13 13:34:20 +0200

    Added Drush alias for the staging environment.

---
+++ sites/default/aliases.drushrc.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file
+ * Site aliases that are utilised by Drush.
+ *
+ * These aliases makes it possible to execute Drush on another site. By adding
+ * the alias in the beginning of the command, the command will be executed on
+ * the other site. E.g. drush @staging features.
+ */
+
+/**
+ * An alias for the staging site.
+ *
+ * This should point to the site on Odd Server, for any normal circumstances.
+ */
+$aliases['staging'] = array(
+  'uri' => 'http://intranat-sedermera.oddserver.se',
+  'root' => '/mnt/persist/www/intranat-sedermera',
+  'remote-host' => 'oddserver.se',
+  'remote-user' => 'root',
+  'command-specific' => array (
+    'sql-sync' => array (
+      'create-db' => TRUE,
+    ),
+  ),
+);
