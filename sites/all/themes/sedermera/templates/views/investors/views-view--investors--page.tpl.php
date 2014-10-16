72e613cd72700da2e7d32f3c3d78122ac3c7a626
Calle <calle.tuvesson@oddhill.se>
2014-10-16 14:44:36 +0200

    Created basic table markup structure

---
+++ sites/all/themes/sedermera/templates/views/investors/views-view--investors--page.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * Main view template.
+ *
+ * If you'd like to override this template, you should create a folder within
+ * this folder and name it to the machine-readable name of the view that you're
+ * overriding. Then have a look at the theme information for that view, and
+ * you'll be able to see which naming convention you should use for the new
+ * file.
+ *
+ * You might end up with something like view_name/views-view--view-name.tpl.php.
+ */
+?>
+<div id="investor-table">
+  <?php print $header; ?>
+  <?php print $exposed; ?>
+  <?php print $attachment_before; ?>
+
+  <div class="table-wrapper">
+    <?php print $rows; ?>
+  </div>
+
+  <?php print $empty; ?>
+  <?php print $pager; ?>
+  <?php print $attachment_after; ?>
+  <?php print $more; ?>
+  <?php print $footer; ?>
+</div>
