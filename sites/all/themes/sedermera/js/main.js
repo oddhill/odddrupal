c0567132be704796bc075bc4edea8df413ea1404
Calle <calle.tuvesson@oddhill.se>
2014-11-03 16:28:29 +0100

    Added styling to company form and image widget
    Image upload widget styling and scripts
    Don't collapse fieldset
    Basic node create form styling

--- sites/all/themes/sedermera/js/main.js
+++ sites/all/themes/sedermera/js/main.js
@@ -20,6 +20,60 @@ if (Drupal.jsAC) {
   };
 }
 
+// Override module javascript and use try catch to isolate errors.
+Drupal.behaviors.form_placeholder = {
+  attach: function(context, settings) {
+    try {
+      var include = settings.form_placeholder.include;
+      if (include) {
+        include += ', ';
+      }
+      include += '.form-placeholder-include-children *';
+      include += ', .form-placeholder-include';
+      var exclude = settings.form_placeholder.exclude;
+      if (exclude) {
+        exclude += ', ';
+      }
+      exclude += '.form-placeholder-exclude-children *';
+      exclude += ', .form-placeholder-exclude';
+
+      var required_indicator = settings.form_placeholder.required_indicator;
+
+      $(include, context).not(exclude).each(function() {
+        $textfield = $(this);
+
+        // Check if element is a textfield.
+        if (!$textfield.is('input[type=text], input[type=email], input[type=password], textarea')) {
+          return;
+        }
+        // Placeholder is supported.
+        else if (Drupal.form_placeholder.placeholderIsSupported() || settings.form_placeholder.fallback_support) {
+          $form = $textfield.closest('form');
+          $label = $form.find('label[for=' + this.id + ']');
+
+          if (required_indicator === 'append') {
+            $label.find('.form-required').insertAfter($textfield).prepend('&nbsp;');
+          }
+          else if (required_indicator === 'remove') {
+            $label.find('.form-required').remove();
+          }
+          else if (required_indicator === 'text') {
+            $label.find('.form-required').text('(' + Drupal.t('required') + ')');
+          }
+
+          $textfield.attr('placeholder', $.trim($label.text()));
+          $label.hide();
+
+          // Fallback support for older browsers.
+          if (!Drupal.form_placeholder.placeholderIsSupported() && settings.form_placeholder.fallback_support) {
+            $textfield.placeholder();
+          }
+        }
+      });
+    } catch(err) {}
+  }
+};
+
 filterHeightMatch = function() {
 
   var $target = $('.filtered-table');
@@ -31,7 +85,13 @@ filterHeightMatch = function() {
   if (filterHeight < wrapperHeight) {
     $filter.height(wrapperHeight);
   }
+};
 
+imageFieldChange = function() {
+  $('.image-widget-data input[type=file]').on('change', function (e) {
+    $in = $(this);
+    $in.prev().val($in.val());
+  });
 };
 
 // Run whenever the DOM tree is changed, e.g. through AJAX/AHAH
@@ -49,6 +109,13 @@ Drupal.behaviors.sedermera = {
     $('select').selectBoxIt();
 
     filterHeightMatch();
+
+    imageFieldChange();
+
+    $('.image-widget-data input[type=file]').change(function (e) {
+      $in = $(this);
+      $in.prev().val($in.val());
+    });
 
     // Use uniform to beautify our checkboxes and radio buttons.
     $('input[type="checkbox"], input[type="radio"]').once().uniform();
