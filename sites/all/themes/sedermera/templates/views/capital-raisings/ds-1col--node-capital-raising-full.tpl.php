88d7a23a9d80afa11190c961a3dca7359b3a7ac1
Olof Johansson <olof.johansson@oddhill.se>
2014-11-06 14:45:37 +0100

    Added initial remaining fields for capital raisings.

--- sites/all/themes/sedermera/templates/views/capital-raisings/ds-1col--node-capital-raising-full.tpl.php
+++ sites/all/themes/sedermera/templates/views/capital-raisings/ds-1col--node-capital-raising-full.tpl.php
@@ -25,6 +25,11 @@
   <div class="transaction-information">
     <?php print render($content['field_cap_rais_comp']); ?>
     <?php print render($content['field_cap_rais_date']); ?>
+    <?php print render($content['field_cap_rais_post']); ?>
+    <?php print render($content['field_cap_rais_rate']); ?>
+    <?php print render($content['field_cap_rais_vol']); ?>
+    <?php print render($content['field_cap_rais_stock_no']); ?>
+    <?php print render($content['field_cap_rais_estim']); ?>
     <?php print render($content['field_cap_rais_file']); ?>
   </div>
 
