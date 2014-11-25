47c9f0541598fae13d05790c9dba1b8ca56d02f4
Olof Johansson <olof.johansson@oddhill.se>
2014-11-25 17:06:46 +0100

    Warn the user when leaving the capital raising multistep form.

--- sites/all/modules/custom/sedermera_offer/sedermera_offer.js
+++ sites/all/modules/custom/sedermera_offer/sedermera_offer.js
@@ -36,4 +36,25 @@ Drupal.behaviors.sedermeraOfferStock = {
   }
 };
 
+Drupal.behaviors.sedermeraOfferWarning = {
+  attach: function (context, settings) {
+
+    // Indicates whether the form has been submitted or not.
+    var formSubmitted = false;
+
+    // Change the submitted property ones any form has been submitted.
+    $('form').submit(function() {
+      formSubmitted = true;
+    });
+
+    // Warn the user when leaving the page, if the form wasn't submitted.
+    $(window).bind('beforeunload', function() {
+      if (!formSubmitted) {
+        return 'Om du lämnar sidan kommer transaktionen inte att skapas, och du förlorar all information du har angivit.';
+      }
+    });
+
+  }
+};
+
 })(jQuery);
