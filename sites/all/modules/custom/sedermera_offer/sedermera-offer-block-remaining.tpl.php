0c7653c943e727edbf19db44beb6e050315b90fe
Olof Johansson <olof.johansson@oddhill.se>
2015-01-21 11:12:12 +0100

    Added current guaranteed and committed values to the progress bar block.

--- sites/all/modules/custom/sedermera_offer/sedermera-offer-block-remaining.tpl.php
+++ sites/all/modules/custom/sedermera_offer/sedermera-offer-block-remaining.tpl.php
@@ -4,33 +4,30 @@
  * TODO: Comment and stuff.
  */
 ?>
-<?php if ($guaranteed || $committed): ?>
-<div class="vertical-bars">
-
-  <?php if ($guaranteed): ?>
+<?php if ($guaranteed && $committed): ?>
+  <div class="vertical-bars">
     <div class="guaranteed-wrapper bar">
       <div class="progress vertical bottom guaranteed">
         <div class="progress-bar" data-transitiongoal="<?php print $guaranteed['percentage']; ?>"></div>
       </div>
       <div class="text">Garanterat</div>
     </div>
-  <?php endif; ?>
-
-  <?php if ($committed): ?>
-  <div class="committed-wrapper bar">
-    <div class="progress vertical bottom committed">
-      <div class="progress-bar" data-transitiongoal="<?php print $committed['percentage']; ?>"></div>
+    <div class="committed-wrapper bar">
+      <div class="progress vertical bottom committed">
+        <div class="progress-bar" data-transitiongoal="<?php print $committed['percentage']; ?>"></div>
+      </div>
+      <div class="text">Åtaget</div>
     </div>
-    <div class="text">Åtaget</div>
   </div>
-  <?php endif; ?>
-
-</div>
 <?php endif; ?>
 
 <div class="total-wrapper">
   <div class="progress">
     <div class="progress-bar" data-transitiongoal="<?php print $total['percentage']; ?>"></div>
   </div>
-  <div class="total"><?php print $total['current']; ?> av <?php print $total['max']; ?> SEK</div>
+  <?php if ($guaranteed && $committed): ?>
+    <div class="total">Garanterat: <?php print $guaranteed['current']; ?> av <?php print $guaranteed['max']; ?> SEK</div>
+    <div class="total">Åtaget: <?php print $committed['current']; ?> av <?php print $committed['max']; ?> SEK</div>
+  <?php endif; ?>
+  <div class="total">Totalt: <?php print $total['current']; ?> av <?php print $total['max']; ?> SEK</div>
 </div>
