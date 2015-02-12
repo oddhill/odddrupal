d1068fc987d498f313239022974d04ef835da2de
Hannele <hannele.piirainen@oddhill.se>
2015-02-12 14:57:59 +0100

    Changed acitivity dashboard styling
    Added class to no result behaviour for dashboard activities

--- sites/all/themes/sedermera/templates/views/dashboard/views-view--aktiviteter--dashboard.tpl.php
+++ sites/all/themes/sedermera/templates/views/dashboard/views-view--aktiviteter--dashboard.tpl.php
@@ -12,7 +12,7 @@
  * You might end up with something like view_name/views-view--view-name.tpl.php.
  */
 ?>
-<div class="empty-container <?php print $classes; ?>">
+<div class="<?php print $classes; ?>">
   <?php print $header; ?>
   <?php print $exposed; ?>
   <?php print $attachment_before; ?>
