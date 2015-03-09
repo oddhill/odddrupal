a7e1f9e29dd7343547556289571aded301fec655
Olof Johansson <olof.johansson@oddhill.se>
2015-03-09 18:50:11 +0100

    Added support for viewing a verification for an investor.
    Added permission which lets the users access the viewable verification if the user doesn't have access to edit the verification.
    
    Configured DS settings for investors and verifications.
    
    Added corresponding template files.

---
+++ sites/all/themes/sedermera/templates/investors/ds-1col--investor-company-investor-verification.tpl.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file
+ * Default theme implementation that displays a node using the DS 1 column
+ * layout.
+ *
+ * If you'd like to create a different template file for nodes, you should
+ * create a new DS layout, and place it in the templates/ds folder, just like
+ * this implementation.
+ *
+ * Have a look at the example_layout folder in the DS folder for guidance.
+ */
+?>
+<div class="<?php print $classes; ?> <?php print $ds_content_classes; ?>">
+
+  Identitetskontroll:
+  <?php if ($content['field_shared_veri_id']): ?>
+    <?php print render($content['field_shared_veri_id']); ?>
+  <?php else: ?>
+    Ej genomförd
+  <?php endif; ?>
+
+  Registreringsbevis:
+  <?php if ($content['field_shared_veri_register']): ?>
+    <?php print render($content['field_shared_veri_register']); ?>
+  <?php else: ?>
+    Ej genomförd
+  <?php endif; ?>
+
+  EU:s sanktionsförordningar:
+  <?php if ($content['field_shared_veri_eu']): ?>
+    <?php print render($content['field_shared_veri_eu']); ?>
+  <?php else: ?>
+    Ej genomförd
+  <?php endif; ?>
+
+  Kreditkontroll:
+  <?php if ($content['field_shared_veri_credit']): ?>
+    <?php print render($content['field_shared_veri_credit']); ?>
+  <?php else: ?>
+    Ej genomförd
+  <?php endif; ?>
+
+</div>
