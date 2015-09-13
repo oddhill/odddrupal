582df0e0e874d065866d5a5b2e6530dcf2beb44d
Calle <calle.tuvesson@oddhill.se>
2015-09-13 21:40:50 +0200

    Lead display & styles
    - Display auto generated title when viewing Lead
    - Lead display styling
    - Automatically set lead title to e-mail value and hide title field
    - Layout "Lead" form styling
    - Add layout groups to investor "Lead"
    - Select boxes layout
    - Allow administrators to add, edit and view "Lead"

---
+++ sites/all/themes/sedermera/templates/investors/ds/ds-1col--investor-lead.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file
+ * Default theme implementation that displays a node using the DS 1 column
+ * layout.
+ *
+ * If you'd like to create a different template file for nodes, you should
+ * create a new DS layout, and place it in the templates/ds folder, just like
+ * this implementation.
+ *
+ * Have a look at the example_layout folder in the DS folder for guidance.
+ */
+?>
+<div class="<?php print $classes; ?> <?php print $ds_content_classes; ?> padding bg">
+  <?php print render($content['title']); ?>
+  <div class="columns">
+    <div class="col">
+      <?php print render($content['field_shared_email']); ?>
+      <?php print render($content['field_shared_cell']); ?>
+      <?php print render($content['field_shared_phone']); ?>
+    </div>
+    <div class="col">
+      <?php print render($content['field_shared_notes']); ?>
+    </div>
+  </div>
+</div>
