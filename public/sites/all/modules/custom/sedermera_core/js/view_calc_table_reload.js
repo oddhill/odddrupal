4f0f6513bfc89df9a17e227f903d50c55793321b
Mads Nielsen <mads.nielsen@oddhill.se>
2015-11-09 16:11:33 +0100

    Modify js to fit new views machine name

--- public/sites/all/modules/custom/sedermera_core/js/view_calc_table_reload.js
+++ public/sites/all/modules/custom/sedermera_core/js/view_calc_table_reload.js
@@ -2,10 +2,10 @@
   Drupal.behaviors.sedermeraViewsCalcTableReload = {
     attach: function (context, settings) {
       var views = {
-        erbjudande:['page', 'interest'],
+        transactions:['page'],
       };
       jQuery.each(Drupal.views.instances, function(i, view) {
-        if (view.settings.view_name == "erbjudande") {
+        if (view.settings.view_name == "transactions") {
           if (typeof views[view.settings.view_name] !== 'undefined') {
             if (views[view.settings.view_name].indexOf(view.settings.view_display_id) > -1) {
               var selector = '.view-dom-id-' + view.settings.view_dom_id;
