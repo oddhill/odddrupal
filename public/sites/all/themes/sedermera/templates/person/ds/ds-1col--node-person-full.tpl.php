5319468ac8c528637825960696143d720c236a0e
Calle <calle.tuvesson@oddhill.se>
2015-11-11 13:39:24 +0100

    Full view person styles and settings
    - Person full node view styling
    - Use english machine names only

---
+++ public/sites/all/themes/sedermera/templates/person/ds/ds-1col--node-person-full.tpl.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @file
+ * Default theme implementation that displays a node using the DS 1 column
+ * layout.
+ *
+ * If you'd like to create a different template file for nodes, you should
+ * create a new DS layout, and place it in the templates/ds folder, just like
+ * this implementation.
+ *
+ * Have a look at the example_layout folder in the DS folder for guidance.
+ */
+?>
+<div class="node-barrier">
+  
+  <div class="<?php print $classes; ?> <?php print $ds_content_classes; ?>">
+    <?php print render($content['title']); ?>
+
+    <div class="person-stakeholder node-section">
+      <?php print render($content['investors']); ?>
+    </div>
+
+    <div class="person-details node-section">
+      <?php print render($content['field_user_ssn']); ?>
+      <?php print render($content['field_user_mail']); ?>
+      <?php print render($content['field_user_phone']); ?>
+      <?php print render($content['field_user_cell']); ?>
+      <?php print render($content['field_user_fax']); ?>
+      <?php print render($content['field_user_branches']); ?>
+      <?php print render($content['field_user_interests']); ?>
+    </div>
+
+    <div class="person-other node-section">
+      <?php print render($content['field_user_id_address']); ?>
+      <?php print render($content['field_user_notes']); ?>
+    </div>
+  </div>
+</div>
