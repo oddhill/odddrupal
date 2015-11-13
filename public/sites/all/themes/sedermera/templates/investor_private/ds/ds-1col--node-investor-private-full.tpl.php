2b36e551312ef28f163c2d6fa39a4b5b0411f0c2
Calle <calle.tuvesson@oddhill.se>
2015-11-13 15:33:19 +0100

    Private investor styling

---
+++ public/sites/all/themes/sedermera/templates/investor_private/ds/ds-1col--node-investor-private-full.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file
+ * Default theme implementation that displays a node using the DS 1 column
+ * layout.
+ *
+ * If you'd like to create a different template file for nodes, you should
+ * create a new DS layout, and place it in the templates/ds folder, just like
+ * this implementation.
+ *
+ * Have a look at the example_layout folder in the DS folder for guidance.
+ */
+?>
+<div class="<?php print $classes; ?> <?php print $ds_content_classes; ?>">
+
+  <?php print render($content['verification_status']); ?>
+  <?php print render($content['title']); ?>
+
+  <div class="investor-details node-section">
+    <?php print render($content['field_shared_cust_id']); ?>
+    <?php print render($content['field_shared_cont_pers']); ?>
+    <?php print render($content['field_invest_pvt_ctrl_ua']); ?>
+    <?php print render($content['field_invest_pvt_ctrl_ptv']); ?>
+    <?php print render($content['field_invest_pvt_ctrl_cred']); ?>
+    <?php print render($content['field_invest_pvt_ctrl_ext']); ?>
+    <?php print render($content['field_invest_pvt_ctrl_oth']); ?>
+  </div>
+</div>
