5a5581c1824ef301df2eaac0d926e53acaca42d5
Olof Johansson <olof.johansson@oddhill.se>
2015-11-20 16:07:28 +0100

    Implemented action for the Avtalspart checkbox for Intressenter.
    - Added AJAX callback which will create the node and return the new table.
    - Added custom JS which will fire the GET request and take care of the returned AJAX commands.
    - Fixed argument for the table callback in order to be able to call it from other places.

---
+++ public/sites/all/modules/custom/sedermera_transaction/js/sedermera_transaction_add_ap.js
@@ -0,0 +1,44 @@
+(function($) {
+
+Drupal.behaviors.sedermeraTransactionAddAp = {
+  attach: function(context, settings) {
+
+    $(':checkbox.add-transaction-ap').once('add-transaction-ap').change(function(e) {
+      var $this = $(this);
+
+      // Make sure that the checkbox is checked, just in case.
+      if (!$this.is(':checked')) {
+        return;
+      }
+
+      // Get the transaction and avtalspart IDs.
+      var transaction_id = $this.data('transaction-id');
+      var ap_id = $this.data('ap-id');
+
+      // Insert a throbber in the same way as Drupal does it.
+      var throbberElement = $('<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>');
+      $('.throbber', throbberElement).after('<div class="message">Vänligen vänta...</div>');
+      $this.after(throbberElement);
+
+      // Perform a GET request to the callback which creates the Transaktion AP
+      // node.
+      $.get(settings.basePath + 'transaction/add-ap/' + transaction_id + '/' + ap_id, function(data, success) {
+        var ajax = Drupal.ajax.prototype;
+
+        // Set the AJAX effect.
+        ajax.effect = 'none';
+
+        // Execute every command that got returned.
+        for (var i in data) {
+          ajax.commands[data[i].command](ajax, data[i], status);
+        }
+
+        // Attach behaviors.
+        Drupal.attachBehaviors();
+      });
+    });
+
+  }
+};
+
+}(jQuery));
