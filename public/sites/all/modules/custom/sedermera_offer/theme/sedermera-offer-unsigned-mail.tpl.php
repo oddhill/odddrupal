edeb90c109ce0983ebe456a84175fb4ddfed839c
Hannele <hannele.piirainen@oddhill.se>
2015-11-25 11:54:30 +0100

    Added html mail markup for signed and unsigned offer
    + Also added Sedermera logo for html mail

--- public/sites/all/modules/custom/sedermera_offer/theme/sedermera-offer-unsigned-mail.tpl.php
+++ public/sites/all/modules/custom/sedermera_offer/theme/sedermera-offer-unsigned-mail.tpl.php
@@ -7,13 +7,71 @@
  * - $recipient: Name of the recipient.
  * - $sender: Name of the user that sent the email.
  * - $company: Name of the company for which the information is connected to.
+ *
+ * About HTML-mail:
+ * Don't use comments in the code below - some email clients can choke on comments so please don't put any unnecessary code anywhere in the file.
  */
 ?>
-Hej <?php print $recipient; ?>,
 
-Ni har härmed blivit utloggade från Er insynsställning i <?php print $company; ?>.
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+  <tr>
+   <td>
 
-Om Ni har några frågor eller funderingar, vänligen kontakta mig.
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;">
 
-Med vänlig hälsning
-<?php print $sender; ?>
+      <tr>
+        <td align="center" style="border-bottom: 2px solid #eee; padding-bottom: 30px; padding-top: 30px;">
+          <img src="mail-logo.png" alt="Sedermera Fondkommission" style="display: block;" />
+        </td>
+      </tr>
+
+      <tr>
+        <td bgcolor="#ffffff" style="padding: 30px 20px 30px 20px;">
+
+          <table border="0" cellpadding="0" cellspacing="0" width="100%">
+
+            <tr>
+              <td style="color: #1a1a1a; font-family: Arial, sans-serif; font-size: 13px; padding: 0 0 15px 0;">
+                Hej <?php print $recipient; ?>,
+              </td>
+            </tr>
+
+            <tr>
+             <td style="color: #1a1a1a; font-family: Arial, sans-serif; font-size: 13px; padding: 0 0 15px 0;">
+              Ni har härmed blivit utloggade från Er insynsställning i <?php print $company; ?>.
+             </td>
+            </tr>
+
+            <tr>
+              <td style="color: #1a1a1a; font-family: Arial, sans-serif; font-size: 13px; padding: 0 0 15px 0;">
+                Om Ni har några frågor eller funderingar, vänligen kontakta mig.
+              </td>
+            </tr>
+
+            <tr>
+              <td style="color: #1a1a1a; font-family: Arial, sans-serif; font-size: 13px; padding: 0 0 15px 0;">
+                Med vänlig hälsning
+              </td>
+            </tr>
+
+            <tr>
+              <td style="color: #1a1a1a; font-family: Arial, sans-serif; font-size: 13px;">
+                <?php print $sender; ?>
+              </td>
+            </tr>
+
+          </table>
+
+        </td>
+      </tr>
+
+      <tr>
+        <td style="border-top: 2px solid #eee; padding-bottom: 20px; padding-top: 5px;">
+        </td>
+      </tr>
+
+    </table>
+
+   </td>
+  </tr>
+</table>
