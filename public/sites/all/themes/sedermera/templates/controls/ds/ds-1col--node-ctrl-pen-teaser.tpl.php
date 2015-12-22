fea42ecf51588765b5d18452a5b71b304358229d
Olof Johansson <olof.johansson@oddhill.se>
2015-12-22 11:39:02 +0100

    Fixed rendering of ctrl_pen nodes.
    - Enabled and configured the teaser view mode.
    - Copied template file and styling from regular ctrl nodes.
    - Adjusted logics for the ctrl_name field in order to support ctrl_pen nodes.

---
+++ public/sites/all/themes/sedermera/templates/controls/ds/ds-1col--node-ctrl-pen-teaser.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * Default theme implementation that displays a node using the DS 1 column
+ * layout.
+ *
+ * If you'd like to create a different template file for nodes, you should
+ * create a new DS layout, and place it in the templates/ds folder, just like
+ * this implementation.
+ *
+ * Have a look at the example_layout folder in the DS folder for guidance.
+ */
+?>
+<div class="<?php print $classes; ?> <?php print $ds_content_classes; ?> <?php print $ver_status; ?>">
+  <div class="inner-wrapper">
+
+    <?php print render($content['ctrl_name']); ?>
+
+    <div class="verification-status">
+      Kontrollen är <span class="ctrl-verification <?php print $ver_status; ?>"><?php print $ver_text; ?></span>
+    </div>
+  </div>
+</div>
