15e784728133292324ab4fd963c0f75575a4409a
Olof Johansson <olof.johansson@oddhill.se>
2015-12-22 20:47:17 +0100

    Fixed missing classes for embeded person view.

--- public/sites/all/themes/sedermera/templates/person/view/views-view--person--offer-embed.tpl.php
+++ public/sites/all/themes/sedermera/templates/person/view/views-view--person--offer-embed.tpl.php
@@ -12,7 +12,7 @@
  * You might end up with something like view_name/views-view--view-name.tpl.php.
  */
 ?>
-<div class="add-persons-list">
+<div class="add-persons-list <?php print $classes; ?>">
   <?php print $header; ?>
 
   <div class="view-filters table-filter">
