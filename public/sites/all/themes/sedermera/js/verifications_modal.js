b7d90267788fdcb0fa6d4db0c5459a33dce99f5c
Olof Johansson <olof.johansson@oddhill.se>
2016-05-23 10:38:46 +0200

    Use ajaxForm instead of ajaxSubmit for forms in modals.
    This will make sure that we'll trigger the appropriate submit handlers.

--- public/sites/all/themes/sedermera/js/verifications_modal.js
+++ public/sites/all/themes/sedermera/js/verifications_modal.js
@@ -47,54 +47,69 @@ Drupal.behaviors.sedermeraVerificationsModal = {
           Drupal.attachBehaviors();
 
           // Submit the form via AJAX when clicking the submit button.
-          $(this.content).find('.form-actions :submit').click(function(event) {
-            event.preventDefault();
+          $(this.content).ajaxForm({
+            beforeSubmit: function(formData, $form) {
+              // Iterate through the submitted data in order to find the submit
+              // button that was pressed.
+              for (var i = 0; i < formData.length; i++) {
+                if (formData[i].name != 'op') {
+                  // We're only interested in the "op" element since that
+                  // represents the clicked button.
+                  continue;
+                }
 
-            // Submit the form.
-            $(this).closest('form').ajaxSubmit({
-              beforeSubmit: function(formData, $form) {
-                // Add a throbber before the form is submitted.
+                // Find the element based on the value. Exit if we can't find
+                // one.
+                $submitElement = $form.find('[name="op"][value="' + formData[i].value + '"]');
+                if (!$submitElement.length) {
+                  break;
+                }
+
+                // Create the throbber element and add it after the clicked
+                // button.
                 var throbberElement = $('<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>');
                 $('.throbber', throbberElement).after('<div class="message">Vänligen vänta...</div>');
-                $form.find('.form-actions :submit').after(throbberElement);
-              },
-              success: function(responseText) {
-                // Reload the current page by performing a GET request to the
-                // current path, appended with /reload. This assumes that we're
-                // currently displaying one of the lists that supports a reload.
-                // See sedermera_transaction_menu() for further details.
-                $.get(window.location.pathname + '/reload', function(data) {
-                  var ajax = Drupal.ajax.prototype;
+                $submitElement.after(throbberElement);
 
-                  // Set the AJAX effect.
-                  ajax.effect = 'none';
-
-                  // Execute every command that got returned.
-                  for (var i in data) {
-                    ajax.commands[data[i].command](ajax, data[i], status);
-                  }
-
-                  // Attach behaviors.
-                  Drupal.attachBehaviors();
-                });
-
-                // Define the new content of the modal.
-                var content = '<div class="verification-body"><div class="success"><p>Ändringarna sparades.</p></div></div><div class="verification-footer"><button title="Close (Esc)" type="button" class="modal-close">OK</button></div>';
-
-                // Open a new modal with the content.
-                $.magnificPopup.open({
-                  items: {
-                    src: content,
-                    type: 'inline'
-                  }
-                });
-
-                // Close modal when clicking "OK" button
-                $('.modal-close').on('click', function() {
-                  $.magnificPopup.close();
-                });
+                // Stop looking for further elements.
+                break;
               }
-            });
+            },
+            success: function(responseText) {
+              // Reload the current page by performing a GET request to the
+              // current path, appended with /reload. This assumes that we're
+              // currently displaying one of the pages that supports a reload.
+              $.get(window.location.pathname + '/reload', function(data) {
+                var ajax = Drupal.ajax.prototype;
+
+                // Set the AJAX effect.
+                ajax.effect = 'none';
+
+                // Execute every command that got returned.
+                for (var i in data) {
+                  ajax.commands[data[i].command](ajax, data[i], status);
+                }
+
+                // Attach behaviors.
+                Drupal.attachBehaviors();
+              });
+
+              // Define the new content of the modal.
+              var content = '<div class="verification-body"><div class="success"><p>Ändringarna sparades.</p></div></div><div class="verification-footer"><button title="Close (Esc)" type="button" class="modal-close">OK</button></div>';
+
+              // Open a new modal with the content.
+              $.magnificPopup.open({
+                items: {
+                  src: content,
+                  type: 'inline'
+                }
+              });
+
+              // Close modal when clicking "OK" button
+              $('.modal-close').on('click', function() {
+                $.magnificPopup.close();
+              });
+            }
           });
         }
       }
