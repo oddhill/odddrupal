1331ba6e1a326ed2ba6e072a250ab9cf870a912a
Olof Johansson <olof.johansson@oddhill.se>
2016-06-13 08:54:40 +0200

    Trying verbose output for everything.

--- build/deploy_testing.sh
+++ build/deploy_testing.sh
@@ -28,16 +28,16 @@ NEW_SETTINGS_FILE=$NEW_DOCROOT/$SETTINGS_FILE
 
 # Move configuration to the appropriate location.
 echo "Bundling .htaccess"
-mv config/.htaccess.testing public/.htaccess
+mv -v config/.htaccess.testing public/.htaccess
 
 # Remove any Drush alias files.
 echo "Removing Drush aliases from sites/default"
-rm public/sites/default/aliases.*
+rm -v public/sites/default/aliases.*
 
 # Create a tarball and save it as an artifact.
 echo "Creating tarball"
-tar czf $BUILD_NAME -C public/ .
-mv $BUILD_NAME $CIRCLE_ARTIFACTS
+tar czvf $BUILD_NAME -C public/ .
+mv -v $BUILD_NAME $CIRCLE_ARTIFACTS
 
 # Copy the tarball to the remote server.
 echo "Uploading tarball to $BASE_PATH"
@@ -47,17 +47,17 @@ scp $CIRCLE_ARTIFACTS/$BUILD_NAME $SSH_URL:$BASE_PATH
 ssh $SSH_URL /bin/bash << EOF
   # Create the new docroot and extract the build to that folder.
   echo "Extracting tarball to $NEW_DOCROOT"
-  mkdir $NEW_DOCROOT
-  tar xzof $BASE_PATH/$BUILD_NAME -C $NEW_DOCROOT
+  mkdir -v $NEW_DOCROOT
+  tar xzvof $BASE_PATH/$BUILD_NAME -C $NEW_DOCROOT
 
   # Copy files and settings from the current docroot to the new one, if the
   # current directory exists, which won't be the case if this is a fresh deploy.
   if [ -d "$CURRENT_DOCROOT" ]; then
     echo "Copying files from $CURRENT_FILE_DIR to $NEW_FILE_DIR"
-    cp -r $CURRENT_FILE_DIR/* $NEW_FILE_DIR/
+    cp -rv $CURRENT_FILE_DIR/* $NEW_FILE_DIR/
 
     echo "Copying $CURRENT_SETTINGS_FILE to $NEW_SETTINGS_FILE"
-    cp $CURRENT_SETTINGS_FILE $NEW_SETTINGS_FILE
+    cp -v $CURRENT_SETTINGS_FILE $NEW_SETTINGS_FILE
   else
     echo "$CURRENT_DOCROOT doesn't exist. You will need to setup $CURRENT_SETTINGS_FILE manually."
   fi
@@ -66,30 +66,30 @@ ssh $SSH_URL /bin/bash << EOF
   # owner. Directories will get 750 since these should be executable, and files
   # will get 640.
   echo "Making $NEW_DOCROOT writable for the owner only"
-  find $NEW_DOCROOT -type d -print0 | xargs -0 chmod 750
-  find $NEW_DOCROOT -type f -print0 | xargs -0 chmod 640
+  find $NEW_DOCROOT -type d -print0 | xargs -0 chmod -v 750
+  find $NEW_DOCROOT -type f -print0 | xargs -0 chmod -v 640
 
   # Make sure that the files directory is writable.
   echo "Making $NEW_FILE_DIR writable by the owner and group"
-  chmod 770 $NEW_FILE_DIR
-  find $NEW_FILE_DIR -type d -print0 | xargs -0 chmod 770
-  find $NEW_FILE_DIR -type f -print0 | xargs -0 chmod 660
+  chmod -v 770 $NEW_FILE_DIR
+  find $NEW_FILE_DIR -type d -print0 | xargs -0 chmod -v 770
+  find $NEW_FILE_DIR -type f -print0 | xargs -0 chmod -v 660
 
   # Change group ownership to the user that is running the web server.
   echo "Changing group ownership of $NEW_DOCROOT to $WWW_USER"
-  chgrp -R $WWW_USER $NEW_DOCROOT
+  chgrp -Rv $WWW_USER $NEW_DOCROOT
 
   # Replace the current docroot with the new one, if the current directory
   # exists.
   if [ -d "$CURRENT_FILE_DIR" ]; then
     echo "Replacing $CURRENT_DOCROOT with $NEW_DOCROOT"
-    rm -r $CURRENT_DOCROOT && mv $NEW_DOCROOT $CURRENT_DOCROOT
+    rm -rv $CURRENT_DOCROOT && mv $NEW_DOCROOT $CURRENT_DOCROOT
   else
     echo "Renaming $NEW_DOCROOT to $CURRENT_DOCROOT"
-    mv $NEW_DOCROOT $CURRENT_DOCROOT
+    mv -v $NEW_DOCROOT $CURRENT_DOCROOT
   fi
 
   # Remove the build.
   echo "Removing tarball"
-  rm $BASE_PATH/$BUILD_NAME
+  rm -v $BASE_PATH/$BUILD_NAME
 EOF
