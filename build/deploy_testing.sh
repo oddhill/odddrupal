602f7a43eb4974b05b300efa5feb41171db67d45
Olof Johansson <olof.johansson@oddhill.se>
2016-06-13 07:56:47 +0200

    Copy settings.local.php from the current directory.
    Don't store the settings within the repo since they contain db credentials.

--- build/deploy_testing.sh
+++ build/deploy_testing.sh
@@ -3,12 +3,13 @@ CURRENT_DOCROOT=$BASE_PATH/portal-sedermera
 NEW_DOCROOT=$BASE_PATH/portal-sedermera-new
 CURRENT_FILE_DIR=$CURRENT_DOCROOT/sites/all/files
 NEW_FILE_DIR=$NEW_DOCROOT/sites/all/files
+CURRENT_SETTINGS_FILE=$CURRENT_DOCROOT/sites/default/settings.local.php
+NEW_SETTINGS_FILE=$NEW_DOCROOT/sites/default/settings.local.php
 WWW_USER=www-data
 BUILD_NAME=build.tar.gz
 
 # Move configuration to the appropriate location.
-echo "Bundling .htaccess and settings.local.php"
-mv config/settings.testing.php public/sites/default/settings.local.php
+echo "Bundling .htaccess"
 mv config/.htaccess.testing public/.htaccess
 
 # Remove any Drush alias file and the default settings file.
@@ -38,13 +39,17 @@ ssh root@oddserver.se /bin/bash << EOF
   find $NEW_DOCROOT -type d -print0 | xargs -0 chmod 750
   find $NEW_DOCROOT -type f -print0 | xargs -0 chmod 640
 
-  # Copy files from the current docroot to the new one, if the current directory
-  # exists, which won't be the case if this is a fresh deploy.
+  # Copy files and settings from the current docroot to the new one, if the
+  # current directory exists, which won't be the case if this is a fresh deploy.
   if [ -d "$CURRENT_FILE_DIR" ]; then
     echo "Copying files from $CURRENT_FILE_DIR to $NEW_FILE_DIR"
     cp -r $CURRENT_FILE_DIR/* $NEW_FILE_DIR/
+
+    echo "Copying $CURRENT_SETTINGS_FILE to $NEW_SETTINGS_FILE"
+    cp $CURRENT_SETTINGS_FILE $NEW_SETTINGS_FILE
   else
     echo "Not copying any files to $NEW_FILE_DIR since $CURRENT_FILE_DIR doesn't exist"
+    echo "Not copying $CURRENT_SETTINGS_FILE to $NEW_SETTINGS_FILE since $CURRENT_FILE_DIR doesn't exist"
   fi
 
   # Make sure that the files directory is writable.
